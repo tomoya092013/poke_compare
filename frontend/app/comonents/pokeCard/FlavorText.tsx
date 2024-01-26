5d0ee482e9c848ac7bb3b4402d183e9c466f3869
tomoya <cj.0s0ttw.nsm4@gmail.com>
2024-01-26 22:48:39 +0900

    高さ調整

--- frontend/app/comonents/pokeCard/FlavorText.tsx
+++ frontend/app/comonents/pokeCard/FlavorText.tsx
@@ -1,15 +1,18 @@
-import { Box, styled } from '@mui/material';
+import { Box, Stack, styled } from '@mui/material';
 
 type Props = {
   flavorText?: string;
 };
 
-const FlavorTextArea = styled(Box)({
+const FlavorTextArea = styled(Stack)({
   width: '230px',
+  height: '142px',
   marginTop: '8px',
   padding: '8px',
   borderRadius: '15px',
   border: '7px double #FFE6CA',
+  alignItems: 'center',
+  justifyContent: 'center',
 });
 
 const FlavorText = ({ flavorText }: Props) => {
